83bbf4855527d8871f3da842bab0209d6a3250d1
pihentagyu <jdoepp@gmail.com>
2019-07-27 10:07:42 -0400

    Models tests

--- internet_speed/models.py
+++ internet_speed/models.py
@@ -2,22 +2,22 @@ from django.db import models
 
 
 class PostcodeArea(models.Model):
-   postcoode_area = models.CharField(max_length=2)
+   postcode_area = models.CharField(max_length=2)
 
    def add_postcodes(self, postcode_dict_list):
         '''Method for populating the FixedPostcode model for a given postcode area'''
-        postcode_list = [FixedPostcode(doc=self, **postcode) for postcode in postcode_dict_list]
-        self.sentence_set.bulk_create(postcode_list)
+        postcode_list = [FixedPostcode(pc_area=self, **postcode) for postcode in postcode_dict_list]
+        self.fixedpostcode_set.bulk_create(postcode_list)
 
 class FixedPostcode(models.Model):
     '''Fixed Postcode table'''
     postcode = models.CharField(max_length=7)
     structured_pc = models.CharField(max_length=8)
     pc_area = models.ForeignKey(PostcodeArea, on_delete=models.CASCADE)
-    med_dld_speed = models.SmallIntegerField()
-    avg_dld_speed = models.SmallIntegerField()
-    med_upld_speed = models.SmallIntegerField()
-    avg_upld_speed = models.SmallIntegerField()
+    med_dld_speed = models.DecimalField(max_digits=4, decimal_places=1)
+    avg_dld_speed = models.DecimalField(max_digits=4, decimal_places=1)
+    med_upld_speed = models.DecimalField(max_digits=4, decimal_places=1)
+    avg_upld_speed = models.DecimalField(max_digits=4, decimal_places=1)
     created = models.DateTimeField(auto_now_add=True)
 
     
--- internet_speed/tests.py
+++ internet_speed/tests.py
@@ -1,3 +1,66 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.utils import timezone
 
-# Create your tests here.
+from .models import *
+from .views import *
+
+class PostcodeAreaTests(TestCase):
+    def setUp(self):
+        self.postcode_area = PostcodeArea.objects.create(
+                postcode_area = 'AB',
+            )
+
+        self.postcode_area.save()
+        self.postcode_dict_list = [{'postcode': 'AB101AL',
+          'structured_pc': 'AB10 1AL',
+          'med_dld_speed': -1.0,
+          'avg_dld_speed': -1.0,
+          'med_upld_speed': -1.0,
+          'avg_upld_speed': -1.0},
+         {'postcode': 'AB101AN',
+          'structured_pc': 'AB10 1AN',
+          'med_dld_speed': -1.0,
+          'avg_dld_speed': -1.0,
+          'med_upld_speed': -1.0,
+          'avg_upld_speed': -1.0},
+         {'postcode': 'AB101AP',
+          'structured_pc': 'AB10 1AP',
+          'med_dld_speed': -1.0,
+          'avg_dld_speed': -1.0,
+          'med_upld_speed': -1.0,
+          'avg_upld_speed': -1.0},
+         {'postcode': 'AB101AS',
+          'structured_pc': 'AB10 1AS',
+          'med_dld_speed': -1.0,
+          'avg_dld_speed': -1.0,
+          'med_upld_speed': -1.0,
+          'avg_upld_speed': -1.0},
+         {'postcode': 'AB101AU',
+          'structured_pc': 'AB10 1AU',
+          'med_dld_speed': 15.7,
+          'avg_dld_speed': 15.5,
+          'med_upld_speed': 1.1,
+          'avg_upld_speed': 1.0}]
+        self.postcode = FixedPostcode.objects.create(
+                pc_area = self.postcode_area,
+                **self.postcode_dict_list[0]
+                )
+        self.postcode.save()
+
+    def test_postcode_area_creation(self):
+        postcode_area = PostcodeArea.objects.last()
+        self.assertEqual(PostcodeArea.objects.last().postcode_area, 'AB')
+
+    def test_postcode_creation(self):
+        self.assertLess(self.postcode.created, timezone.now())
+        postcodes = [v['postcode'] for v in self.postcode_area.fixedpostcode_set.all().values()]
+        self.assertIn('AB101AL', postcodes)
+
+    def test_add_postcodes(self):
+        FixedPostcode.objects.all().delete()
+        self.postcode_area.add_postcodes(self.postcode_dict_list)
+        postcodes = FixedPostcode.objects.all()
+        self.assertEqual(postcodes[1].postcode, 'AB101AN')
+        self.assertEqual(postcodes[4].avg_dld_speed, 15.5)
+        self.assertEqual(postcodes[0].pc_area.postcode_area,'AB')
+
