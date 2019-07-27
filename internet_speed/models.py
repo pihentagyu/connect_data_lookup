bb186922f7cb1df8ba89e90a5dec670df8d1e2eb
pihentagyu <jdoepp@gmail.com>
2019-07-27 00:37:07 -0400

    Created models for postcode data

--- internet_speed/models.py
+++ internet_speed/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class PostcodeArea(models.Model):
+   postcoode_area = models.CharField(max_length=2)
+
+   def add_postcodes(self, postcode_dict_list):
+        '''Method for populating the FixedPostcode model for a given postcode area'''
+        postcode_list = [FixedPostcode(doc=self, **postcode) for postcode in postcode_dict_list]
+        self.sentence_set.bulk_create(postcode_list)
+
+class FixedPostcode(models.Model):
+    '''Fixed Postcode table'''
+    postcode = models.CharField(max_length=7)
+    structured_pc = models.CharField(max_length=8)
+    pc_area = models.ForeignKey(PostcodeArea, on_delete=models.CASCADE)
+    med_dld_speed = models.SmallIntegerField()
+    avg_dld_speed = models.SmallIntegerField()
+    med_upld_speed = models.SmallIntegerField()
+    avg_upld_speed = models.SmallIntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    
+
